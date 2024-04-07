ba0fce76a38190fba475a0960c98aa26638d8a87
Barr <barr520@gmail.com>
2024-04-07 20:38:27 +0300

    q2 almost done

---
+++ Q2/preprocess.py
@@ -0,0 +1,21 @@
+from glob import glob
+import nltk
+from nltk.corpus import stopwords
+import pandas as pd
+
+nltk.download("stopwords")
+nltk.download("punkt")
+stop_words = stopwords.words("english")
+
+data = []
+for file_name in glob("PoliticalBias/*.txt"):
+    with open(file_name, "r") as file:
+        text = file.read()
+        data.append({"label": "poli", "text": text})
+
+for file_name in glob("NewsFairness/*.txt"):
+    with open(file_name, "r") as file:
+        text = file.read()
+        data.append({"label": "news", "text": text})
+data = pd.DataFrame(data)
+data.to_parquet("data.pk")
---
+++ Q2/q2.py
@@ -0,0 +1,73 @@
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+import pandas as pd
+import statistics
+
+kf = KFold(10, shuffle=True)
+
+data = pd.read_parquet("data.pk")
+
+
+def evaluate_classifier(pipeline_func) -> tuple[list, float, pd.DataFrame]:
+    scores = []
+    mispredictions = []
+    for train_idx, test_idx in kf.split(data):
+        classifier: Pipeline = pipeline_func()
+        train_data = data.iloc[train_idx]
+        classifier.fit(train_data["text"], train_data["label"])
+        test_data = data.iloc[test_idx]
+        predictions = classifier.predict(test_data["text"])
+        score = np.mean(predictions == test_data["label"])
+        mispredictions.append(
+            test_data.iloc[np.where(predictions != test_data["label"])[0]]
+        )
+        scores.append(score)
+    mispredictions = pd.concat(mispredictions)
+    return scores, statistics.mean(scores), mispredictions
+
+
+def nb_pipeline():
+    return Pipeline(
+        [("tf", TfidfVectorizer(stop_words="english")), ("multi_nb", MultinomialNB())]
+    )
+
+
+def sgd_pipeline():
+    return Pipeline(
+        [("tf", TfidfVectorizer(stop_words="english")), ("sgd", SGDClassifier())]
+    )
+
+
+def knn_pipeline():
+    return Pipeline(
+        [("tf", TfidfVectorizer(stop_words="english")), ("knn", KNeighborsClassifier())]
+    )
+
+
+def forest_pipeline():
+    return Pipeline(
+        [
+            ("tf", TfidfVectorizer(stop_words="english")),
+            ("forest", RandomForestClassifier()),
+        ]
+    )
+
+
+scores_nb, avg, mispredictions_nb = evaluate_classifier(nb_pipeline)
+print("nb:\n", scores_nb, avg)
+mispredictions_nb.to_html("nb_mispredictions.html")
+scores_sgd, avg, mispredictions_sgd = evaluate_classifier(sgd_pipeline)
+print("sgd:\n", scores_sgd, avg)
+mispredictions_sgd.to_html("sgd_mispredictions.html")
+scores_knn, avg, mispredictions_knn = evaluate_classifier(knn_pipeline)
+print("knn:\n", scores_knn, avg)
+mispredictions_knn.to_html("knn_mispredictions.html")
+scores_forest, avg, mispredictions_forest = evaluate_classifier(forest_pipeline)
+print("forest:\n", scores_forest, avg)
+mispredictions_forest.to_html("forest_mispredictions.html")
