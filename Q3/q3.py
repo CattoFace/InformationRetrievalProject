5b596e6c1bbd9ac046297f846a01e45709069b33
Barr <barr520@gmail.com>
2024-04-13 21:22:14 +0300

    final

--- Q3/q3.py
+++ Q3/q3.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
 from sklearn.model_selection import KFold
 from sklearn.metrics import rand_score, ConfusionMatrixDisplay
 import pandas as pd
@@ -11,7 +12,6 @@ np.random.seed(42)
 kf = KFold(10, shuffle=True)
 
 data = pd.read_parquet("data.pk")
-
 
 # used in interactive mode to see common words
 def common_in_docs(docs):
@@ -37,4 +37,19 @@ score = rand_score(result, data["label"])
 print("rand score: ", score)
 ConfusionMatrixDisplay.from_predictions(data["label"].tolist(), predictions)
 plt.title("Confusion Matrix")
+plt.show()
+embedded = TSNE(init="random", random_state=42).fit_transform(classifier.steps[0][1].transform(data["text"]))
+plt.scatter(embedded[data["label"]=="poli",0],embedded[data["label"]=="poli",1],c="red", label="Political Bias")
+plt.scatter(embedded[data["label"]=="news",0],embedded[data["label"]=="news",1],c="blue", label="News Fairness")
+plt.scatter(embedded[data["label"]=="soci",0],embedded[data["label"]=="soci",1],c="green", label="Social Bias")
+plt.scatter(embedded[data["label"]=="miti",0],embedded[data["label"]=="miti",1],c="yellow", label="Bias Mitigation")
+plt.legend(loc="upper right")
+plt.title("By Label")
+plt.show()
+plt.scatter(embedded[result==0,0],embedded[result==0,1],c="red", label="Cluster 0")
+plt.scatter(embedded[result==1,0],embedded[result==1,1],c="blue", label="Cluster 1")
+plt.scatter(embedded[result==2,0],embedded[result==2,1],c="green", label="Cluster 2")
+plt.scatter(embedded[result==3,0],embedded[result==3,1],c="yellow", label="Cluster 3")
+plt.legend(loc="upper right")
+plt.title("By Cluster")
 plt.show()
